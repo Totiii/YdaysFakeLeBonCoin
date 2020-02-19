6105337537327f86f0be85bc4ab682307fad68f0
Toti <tpommier562gmail.com>
2020-02-19 17:28:44 +0100

    nearly nothing cause i'm dumb

--- src/Controller/AnnouncementController.php
+++ src/Controller/AnnouncementController.php
@@ -4,8 +4,26 @@ namespace App\Controller;
 
 use App\Entity\Announcement;
 use App\Entity\Category;
+use App\Entity\City;
+use App\Entity\Picture;
+use App\Form\AnnouncementType;
+use App\Form\CityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
 
 class AnnouncementController extends AbstractController
 {
@@ -20,15 +38,85 @@ class AnnouncementController extends AbstractController
     }
 
     /**
-     *@Route("/newAnnouncement", name="newAnnouncement")
+     * @Route("/newAnnouncement", name="newAnnouncement")
+     * @param Request $request
+     * @return Response
      */
-    public function addAnnouncement()
+    /*
+    public function addAnnouncement(Request $request)
     {
+        if($request->isMethod('post')){
+            $category = $request->request->get('category');
+            $title = $request->request->get('title');
+            $description = $request->request->get('description');
+            $price = $request->request->get('price');
+            $city = $request->request->get('city');
+            $postalCode = $request->request->get('postalCode');
+            $address = $request->request->get('address');
+            $post = $request->request->get("add_announcement");
+
+            if($post) {
+                if ($category and $title and $description and $price and $city and $postalCode and $address) {
+                    $em = $this->getDoctrine()->getManager();
+                    $announcement = new Announcement();
+                    $myCategory = $em->getRepository(Category::class)->findOneBy([
+                        'id' => $category
+                    ]);
+                    $myCity = $em->getRepository(City::class)->findOneBy([
+                        'name' => $city,
+                        'postalCode' => $postalCode
+                    ]);
+                    if ($myCity == "") {
+                        $myCity = new City();
+                        $myCity->setName($city);
+                        $myCity->setPostalCode($postalCode);
+                        $em->persist($myCity);
+                    }
+
+                    $announcement->setCategory($myCategory);
+                    $announcement->setName($title);
+                    $announcement->setDescription($description);
+                    $announcement->setPrice($price);
+                    $announcement->setCity($myCity);
+                    $announcement->setAdress($address);
+                    $announcement->setPublishedDate(new \DateTime());
+                    $announcement->setUser($this->getUser());
+                    $em->persist($announcement);
+                    $em->flush();
+
+                    $this->redirectToRoute('addImages');
+                }
+            }
+        }
+
+
         $em = $this->getDoctrine();
-        $repo = $em->getRepository(Category::class)->findAll();
+        $categories = $em->getRepository(Category::class)->findAll();
 
         return $this->render('add.html.twig', [
-            'categories' => $repo
+            'categories' => $categories,
+        ]);
+    }*/
+
+    /**
+     * @Route("/newAnnouncement", name="newAnnouncement")
+     * @param Request $request
+     * @return Response
+     */
+    public function createMovie(Request $request){
+        $announcement = new Announcement();
+        $form = $this->createForm(AnnouncementType::class, $announcement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $announcement->setPublishedDate(new \DateTime());
+            $em->persist($announcement);
+            $em->flush();
+        }
+
+        return $this->render('add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/AnnouncementType.php
+++ src/Form/AnnouncementType.php
@@ -3,7 +3,17 @@
 namespace App\Form;
 
 use App\Entity\Announcement;
+use App\Entity\Category;
+use App\Entity\City;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +22,30 @@ class AnnouncementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Name')
-            ->add('Price')
-            ->add('PublishedDate')
-            ->add('Description')
-            ->add('adress')
-            ->add('category')
-            ->add('city')
-            ->add('user')
-            ->add('users')
+            ->add('Name', TextType::class,
+                ['label' => false]
+            )
+            ->add('Price', NumberType::class,
+                ['label' => false]
+            )
+            ->add('Description',TextareaType::class,
+                ['label' => false]
+            )
+            ->add('adress', TextType::class,
+                ['label' => false]
+            )
+            ->add('category', EntityType::class,
+                [
+                    'class' => Category::class,
+                    'label' => false
+                ]
+            )
+            ->add('city', EntityType::class,
+                [
+                    'class' => City::class,
+                    'label' => false
+                ]
+            )
         ;
     }
 
